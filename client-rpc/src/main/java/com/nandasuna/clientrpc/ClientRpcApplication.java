3264b53ca95b75cf50987147dde8f75e694aecb4
nandasuna <nanda.95.ndp@gmail.com>
2023-11-25 14:47:08 -0300

    feat: add missing methods

--- client-rpc/src/main/java/com/nandasuna/clientrpc/ClientRpcApplication.java
+++ client-rpc/src/main/java/com/nandasuna/clientrpc/ClientRpcApplication.java
@@ -30,10 +30,41 @@ public class ClientRpcApplication {
 		client.setConfig(config);
 
 		// Faz uma chamada remota
-		Object[] params = new Object[]
-			{ 2.0, 3.0 };
-		double result = (double)client.execute("Calculadora.somar", params);
-		System.out.println("40 + 9 = " + result);
+		Object[] params = new Object[] { 2.0, 3.0 };
+		Object[] param = new Object[] { 10.0 };
+		Object[] paramSinCos = new Object[] { 90.0 };
+		Object[] paramRoot = new Object[] { 4.0 };
+
+
+		double resultSoma = (double)client.execute("Calculadora.somar", params);
+		System.out.println("2 + 3 = " + resultSoma);
+
+		double resultSubtracao = (double)client.execute("Calculadora.subtrair", params);
+		System.out.println("2 - 3 =  " + resultSubtracao);
+
+		double resultMultiplicacao = (double)client.execute("Calculadora.multiplicar", params);
+		System.out.println("2 * 3 = " + resultMultiplicacao);
+
+		double resulDivisao = (double)client.execute("Calculadora.dividir", params);
+		System.out.println("2 / 3 = " + resulDivisao);
+
+		double resulLogE = (double)client.execute("Calculadora.logaritmomBaseE", param);
+		System.out.println("log natural 10 = " + resulLogE);
+
+		double resulLog10 = (double)client.execute("Calculadora.logaritmoBase10", param);
+		System.out.println("log 10 na base 10 = " + resulLog10);
+
+		double resulExpo = (double)client.execute("Calculadora.exponenciacao", params);
+		System.out.println("2 ^ 3= " + resulExpo);
+
+		double resulSin = (double)client.execute("Calculadora.seno", paramSinCos);
+		System.out.println("sin 90 = " + resulSin);
+
+		double resulCos = (double)client.execute("Calculadora.cosseno", paramSinCos);
+		System.out.println("cos 90 = " + resulCos);
+
+		double resulRaiz2 = (double)client.execute("Calculadora.raizQuadrada", paramRoot);
+		System.out.println("raiz quadrada de 4 = " + resulRaiz2);
 	
 	}
 
--- server-rpc/src/main/java/com/nandasuna/serverrpc/Calculator.java
+++ server-rpc/src/main/java/com/nandasuna/serverrpc/Calculator.java
@@ -18,4 +18,35 @@ public class Calculator {
 		return x / y;
 	}
 
+	public double logaritmomBaseE(double num) {
+        return Math.log(num);
+    }
+
+    public double logaritmoBase10(double num) {
+        return Math.log10(num);
+    }
+
+    public double exponenciacao(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public double seno(double num) {
+        return Math.sin(Math.toRadians(num));
+    }
+
+    public double cosseno(double num) {
+        return Math.round(Math.cos(Math.toRadians(num)));
+    }
+
+    public double raizQuadrada(double num) {
+        if (num >= 0) {
+            return Math.sqrt(num);
+        } else {
+            // Handle square root of a negative number
+			System.out.println("Não é possivel calcular raiz de numeros negativos");
+            num = Double.NaN; // You may choose a different approach
+			return num;
+        }
+    }
+
 }
